90a37a8722e8664b9214613b4f6822f5b89f1fac
Sh1n0suk3 <97220504+Sh1n0suk3@users.noreply.github.com>
2025-06-15 12:00:47 +0700

    Delete sh_performance.lua

--- sh_performance.lua
+++
@@ -1,3 +0,0 @@
-CreateConVar("performancemod_enabled", "1", FCVAR_ARCHIVE + FCVAR_REPLICATED, "Global switch for performance mod")
-CreateConVar("performancemod_server_optimize_animations", "0", FCVAR_ARCHIVE + FCVAR_REPLICATED, "Enable/disable animation optimization on server")
-CreateConVar("performancemod_server_optimize_memory", "0", FCVAR_ARCHIVE + FCVAR_REPLICATED, "Enable/disable memory optimization on server")
